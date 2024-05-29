ee8a28237151ba202cd889e6dfee95250e52d306
sivasadobe <sivas@adobe.com>
2024-05-29 17:24:25 +0530

    fix

--- .github/workflows/src/helpers.js
+++ .github/workflows/src/helpers.js
@@ -1,19 +1,19 @@
 function convertMarkdownToSlackFormat(markdown) {
   console.log(markdown);
-  // Convert headings
-  markdown = markdown.replace(/^(#+) (.*$)/gim, "*$2*");
+  //   // Convert italic text
+  //   markdown = markdown.replace(/\*(.*?)\*/gim, "_$1_");
   // Convert bold text
   markdown = markdown.replace(/\*\*(.*?)\*\*/gim, "*$1*");
-  // Convert italic text
-  markdown = markdown.replace(/\*(.*?)\*/gim, "_$1_");
   // Convert inline code
   markdown = markdown.replace(/`([^`]+)`/g, "```$1```");
   // Convert block quotes
   markdown = markdown.replace(/^> (.*$)/gim, "_$1_");
   // Convert unordered lists
-  markdown = markdown.replace(/^\s*[-+*]\s+(.*$)/gim, "• $1");
+  markdown = markdown.replace(/^\s*[-+*]\s+(.*$)/gim, "- $1");
   // Convert ordered lists
   markdown = markdown.replace(/^\s*\d+\.\s+(.*$)/gim, "1. $1");
+  // Convert headings
+  markdown = markdown.replace(/^(#+) (.*$)/gim, "*$2*");
   // Convert newlines to break lines
   markdown = markdown.replace(/\n/g, "\n");
   console.log("After Conversion", markdown);
