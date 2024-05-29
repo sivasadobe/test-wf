68791dbc497c959c14428b1dc8da5564de1e05a6
sivasadobe <sivas@adobe.com>
2024-05-29 11:48:10 +0530

    Fix-context

--- .github/workflows/send-slack.js
+++ .github/workflows/send-slack.js
@@ -1,8 +1,10 @@
-const sendSlackNotification = (slackMessage, slack_webhook_url) => {
-  return fetch(slack_webhook_url, {
+const sendSlackNotification = async (slackMessage, slack_webhook_url) => {
+  const result = await fetch(slack_webhook_url, {
     method: "POST",
     body: JSON.stringify(slackMessage),
   }).catch(console.error);
+  console.log("API Response", result);
+  return result;
 };
 
 module.exports = sendSlackNotification;
