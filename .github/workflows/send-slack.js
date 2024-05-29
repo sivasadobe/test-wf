0d135795169e100b4180efca28bd26865a068254
sivasadobe <sivas@adobe.com>
2024-05-29 11:55:36 +0530

    Fix-variable

--- .github/workflows/send-slack.js
+++ .github/workflows/send-slack.js
@@ -2,7 +2,7 @@ const sendSlackNotification = async (slackMessage, slack_webhook_url) => {
   console.log("slackMessage", slackMessage);
   const result = await fetch(slack_webhook_url, {
     method: "POST",
-    body: JSON.stringify(slackMessage),
+    body: JSON.stringify({ text: slackMessage }),
   }).catch(console.error);
   console.log("API Response", result);
   return result;
